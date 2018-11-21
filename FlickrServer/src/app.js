f9c83297e826416dc6103f70c0cec11935c68371
Hari Krishna Duvuri <hk_india@hotmail.com>
2018-11-21 17:38:56 +1100

    Crated Flickr Server and added express, morgan, bodyparser, axios

---
+++ FlickrServer/src/app.js
@@ -0,0 +1,46 @@
+// setting-up initial required packages
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const express = require('express');
+const logger = console;
+
+// initializing express
+const app = express();
+
+// initializing routes
+
+// initializing setting
+app.set('port', process.env.PORT || 3000);
+
+// setting-up middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// setting-up headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+// configure routes
+
+// exception handling
+
+// finally start the server
+app.listen(app.get('port'), ()=>{
+    console.log('server on port ', app.get('port'));
+});
